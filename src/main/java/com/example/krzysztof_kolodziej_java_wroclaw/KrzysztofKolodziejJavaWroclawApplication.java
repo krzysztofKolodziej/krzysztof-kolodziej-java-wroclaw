4349a634f913813b346e4d57fabcc1ee30a0244c
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2025-05-10 00:21:21 +0200

    Init commit

---
+++ src/main/java/com/example/krzysztof_kolodziej_java_wroclaw/KrzysztofKolodziejJavaWroclawApplication.java
@@ -0,0 +1,13 @@
+package com.example.krzysztof_kolodziej_java_wroclaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrzysztofKolodziejJavaWroclawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrzysztofKolodziejJavaWroclawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/krzysztof_kolodziej_java_wroclaw/KrzysztofKolodziejJavaWroclawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.krzysztof_kolodziej_java_wroclaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrzysztofKolodziejJavaWroclawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
